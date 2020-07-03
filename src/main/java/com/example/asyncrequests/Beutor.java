aa2e48a1a37a081679b9281e5d857258dd7f733d
Victor Rentea <victorrentea@gmail.com>
2020-07-03 10:42:38 +0300

    Non-blocking http request processing.

--- src/main/java/com/example/asyncrequests/Beutor.java
+++ src/main/java/com/example/asyncrequests/Beutor.java
@@ -1,9 +1,12 @@
 package com.example.asyncrequests;
 
+import lombok.SneakyThrows;
+import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.*;
 
@@ -20,40 +23,43 @@ public class Beutor {
    }
 
    @GetMapping
-   public String bea() throws ExecutionException, InterruptedException {
+   public DeferredResult<String> bea() throws ExecutionException, InterruptedException {
+      DeferredResult<String> deferred = new DeferredResult<>();
+
       log.info("Vin la bar");
-
-
       CompletableFuture<Bere> futureBere = CompletableFuture.supplyAsync(barman::toarnaBere);
       CompletableFuture<Vodka> futureVodka = CompletableFuture.supplyAsync(barman::toarnaVodka);
-
 
 //      CompletableFuture.allOf(futureBere, futureVodka).thenRun(() -> System.out.println("Sunt gata bauturile"));
 
       log.info("A plecat fata cu comanda");
 
 
+      CompletableFuture<DillyDilly> futureDilly = futureBere.thenCombine(futureVodka, DillyDilly::new);
 
 
+//      Bere bere = futureBere.get();
+//      // pana aici main doarme 1 sec <--- problema
+//      Vodka vodka = futureVodka.get(); // lina asta o executa instantaneu, pentru ca deja e gata vodka, ca dureaza tot 1 sec sa o toarne
 
+//      DillyDilly dilly = futureDilly.get();
 
-      Bere bere = futureBere.get();
-      // pana aici main doarme 1 sec <--- problema
-      Vodka vodka = futureVodka.get(); // lina asta o executa instantaneu, pentru ca deja e gata vodka, ca dureaza tot 1 sec sa o toarne
-
-      DillyDilly dilly = new DillyDilly(bere, vodka);
-
+      futureDilly.thenAccept(dilly -> {
+            deferred.setResult("Savurez " +dilly); // din alt thread
+      });
       log.info("Threadul HTTP requestului este eliberat aici si se intoarce langa fratii lui in pool");
-      return "Savurez " +dilly;
+      return deferred;
    }
 }
 
+@ToString
 class DillyDilly {
    private static final Logger log = LoggerFactory.getLogger(DillyDilly.class);
    private final Bere bere;
    private final Vodka vodka;
 
-   DillyDilly(Bere bere, Vodka vodka) throws InterruptedException {
+   @SneakyThrows
+   DillyDilly(Bere bere, Vodka vodka) {
       Thread.sleep(1000);
       log.info("Amestec cocktail");
       this.bere = bere;
