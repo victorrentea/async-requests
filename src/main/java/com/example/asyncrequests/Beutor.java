1445ddde1670233f5f02453701be08256797204d
Victor Rentea <victorrentea@gmail.com>
2020-07-03 10:33:27 +0300

    Dilly Dilly facut din main()

--- src/main/java/com/example/asyncrequests/Beutor.java
+++ src/main/java/com/example/asyncrequests/Beutor.java
@@ -28,13 +28,35 @@ public class Beutor {
       CompletableFuture<Vodka> futureVodka = CompletableFuture.supplyAsync(barman::toarnaVodka);
 
 
+//      CompletableFuture.allOf(futureBere, futureVodka).thenRun(() -> System.out.println("Sunt gata bauturile"));
+
       log.info("A plecat fata cu comanda");
+
+
+
+
+
 
       Bere bere = futureBere.get();
       // pana aici main doarme 1 sec <--- problema
       Vodka vodka = futureVodka.get(); // lina asta o executa instantaneu, pentru ca deja e gata vodka, ca dureaza tot 1 sec sa o toarne
 
+      DillyDilly dilly = new DillyDilly(bere, vodka);
+
       log.info("Threadul HTTP requestului este eliberat aici si se intoarce langa fratii lui in pool");
-      return "Savurez " + bere + " cu " + vodka;
+      return "Savurez " +dilly;
+   }
+}
+
+class DillyDilly {
+   private static final Logger log = LoggerFactory.getLogger(DillyDilly.class);
+   private final Bere bere;
+   private final Vodka vodka;
+
+   DillyDilly(Bere bere, Vodka vodka) throws InterruptedException {
+      Thread.sleep(1000);
+      log.info("Amestec cocktail");
+      this.bere = bere;
+      this.vodka = vodka;
    }
 }
